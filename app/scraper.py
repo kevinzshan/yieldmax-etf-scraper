9a7b794cb421f0aa5e39938a094ab900366605d8
Kevin Shan <kevinshan11@gmail.com>
2025-08-05 09:31:26 -0600

    Update scraper.py

--- app/scraper.py
+++ app/scraper.py
@@ -11,7 +11,6 @@ DB_PORT = os.environ.get("DB_PORT", 5432)
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
-ETF = "TSLY"
 
 # Intraday file link (redirects to actual CSV)
 DOWNLOAD_URL = "https://www.yieldmaxetfs.com/ym/intraday-file"
